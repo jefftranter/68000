ada475bcccae9d3ea66bce41c59b27b9215f2b12
Jeff Tranter <tranter@pobox.com>
2017-05-13 19:06:05 -0400

    Convert to VASM assembler.

--- testprog/testprog1.s
+++ testprog/testprog1.s
@@ -1,15 +1,13 @@
-# Simple test program for TS2 68000 board
-# Displays a "Hello, world!" message on the console.
-# Calls code in the TS2 monitor ROM.
+; Simple test program for TS2 68000 board
+; Displays a "Hello, world!" message on the console.
+; Calls code in the TS2 monitor ROM.
 
-	.cpu	68000
-        .org	0x00002000         | Located in RAM
+        ORG	$2000              Located in RAM
 
-PSTRING = 0x0000807c               | Monitor routine to display the string pointed to by A4
+PSTRING EQU     $0000807C          Monitor routine to display the string pointed to by A4
 
-START:
-        lea.l    HELLO(%PC),%A4    | Point to message string
-        jsr      PSTRING           | Print it
-        trap     #14               | Breakpoint, return to monitor
+START   LEA.L    HELLO(PC),A4      Point to message string
+        JSR      PSTRING           Print it
+        TRAP     #14               Breakpoint, return to monitor
 
-HELLO: .ascii   "Hello, world!\r\n\0"
+HELLO   DC.B   "Hello, world!\r\n\0"
--- testprog/testprog2.s
+++ testprog/testprog2.s
@@ -1,26 +1,21 @@
-# Simple test program for TS2 68000 board
-# Displays a "Hello, world!" message on the console.
-# Calls code in the TUTOR monitor ROM.
+; Simple test program for TS2 68000 board
+; Displays a "Hello, world!" message on the console.
+; Calls code in the TUTOR monitor ROM.
 
-	.cpu	68000
-        .org	0x00002000         | Located in RAM
+        ORG	$2000                   Located in RAM
 
-# TRAP 14 functions
-OUTPUT = 243
-OUT1CR = 227
-TUTOR  = 228
+; TRAP 14 functions
+OUTPUT  EQU     243
+OUT1CR  EQU     227
+TUTOR   EQU     228
 
-HELLO = 0x00002020
+START   MOVE.L  #HELLO,A5               Start of string
+        MOVE.L  #EOS,A6                 End of string
+        MOVE.B  #OUT1CR,D7              String output function
+        TRAP    #14                     Call TRAP14 handler
 
-START:
-        move.l   #HELLO,%A5        | Start of string
-        move.l   #HELLO+13,%A6     | End of string
-#       move.b  #OUTPUT,%D7        | String output function
-        move.b  #OUT1CR,%D7        | String output function
-        trap    #14                | Call TRAP14 handler
+        MOVE.B  #TUTOR,D7               Go to TUTOR function
+        TRAP    #14                     Call TRAP14 handler
 
-        move.b  #TUTOR,%D7         | Go to TUTOR function
-        trap    #14                | Call TRAP14 handler
-
-        .org 0x00002020
-        .ascii   "Hello, world!"
+HELLO   DC.B                           "Hello, world!"
+EOS
