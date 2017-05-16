7a92e0638f96b71ea1d035c684f08d14ede6e99d
Jeff Tranter <tranter@pobox.com>
2017-05-16 19:17:29 -0400

    Start entering chapter 5 examples.

---
+++ leventhal/ch05/pgm_5_1a.asm
@@ -0,0 +1,23 @@
+DATA     EQU     $6000
+PROGRAM  EQU     $4000
+
+         ORG     DATA
+LENGTH   DS.W    1               NUMBER OF DATA ELEMENTS
+START    DS.L    1               ADDRESS OF DATA ELEMENTS
+TOTAL    DS.W    1               SUM OF DATA ELEMENTS
+
+         ORG     PROGRAM
+
+PGM_5_1A MOVEA.L START,A0        INITIALIZE POINTER REGISTER
+         MOVEQ   #0,D0           INITIALIZE SUM TO ZERO
+         MOVE.W  LENGTH,D1       INITIALIZE ELEMENT COUNT
+
+LOOP     ADD.W   (A0)+,D0        SUM NEXT ELEMENT
+         SUBQ.W  #1,D1           UPDATE ELEMENT COUNT
+         BNE     LOOP            IF COUNT NOT ZERO THEN GOTO LOOP
+
+         MOVE.W  D0,TOTAL        STORE SUMMATION
+
+         RTS
+
+         END     PGM_5_1A
