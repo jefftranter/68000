9a6845b74cfc95129f8654a7f3ce87ac4ef91ad7
Jeff Tranter <tranter@pobox.com>
2017-05-20 20:38:04 -0400

    Add more examples.

---
+++ leventhal/ch08/pgm_8_3a.asm
@@ -0,0 +1,16 @@
+DATA     EQU     $6000
+PROGRAM  EQU     $4000
+
+NUM1     DS.W    1                      16-BIT MULTIPLICANT
+NUM2     DS.W    1                      16-BIT MULTIPLIER
+RESULT   DS.L    1                      32-BIT MULTIPLICATION RESULT
+
+         ORG     PROGRAM
+
+PGM_8_3A MOVE.W  NUM1,D0                MULTIPLICAND
+         MULU    NUM2,D0                UNSIGNED MULTIPLICATION
+         MOVE.L  D0,RESULT              STORE 32-BIT MULTIPLICATION RESULT
+
+         RTS
+
+         END     PGM_8_3A
