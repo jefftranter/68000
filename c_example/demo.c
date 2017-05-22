570bacce7e12fb838f94959f69ddcf11a7751fcc
Jeff Tranter <tranter@pobox.com>
2017-05-22 18:35:12 -0400

    Add C example.

---
+++ c_example/demo.c
@@ -0,0 +1,29 @@
+/*
+
+  Example of compiling C code for 68000. Doesn't require any C library
+  or other run-time code.
+
+*/
+
+
+short factorial(short n) {
+    if (n <= 0) {
+        return 1;
+    } else {
+       return(n * factorial(n - 1));
+    }
+}
+
+int main()
+{
+    short i = 1;
+    short j = 2;
+    char  c = 'A';
+    long  l = 1234;
+    char *s = "Hello";
+
+    for (i = 1; i < 8; i++) {
+        j = factorial(i);
+    }
+    return j;
+}
