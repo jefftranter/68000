066f377a36ae343cafb8234bfc3c64f6fc72a0ef
Jeff Tranter <tranter@pobox.com>
2017-07-10 17:38:03 -0400

    Make some small improvements.

--- testprog/testprog4.s
+++ testprog/testprog4.s
@@ -1,21 +1,25 @@
-; Example of extending TUTOR trap #14 functions with a custom
-; function. See the TUTOR documentation for details.
+; Example of extending TUTOR trap #14 functions with custom functions.
+; See the TUTOR documentation for details.
 
-        ORG	$2000                   Located in RAM
+        ORG	$2000                   Locate in RAM.
 
 ; TRAP 14 functions
 ADD     EQU     1                       My ADD custom trap function.
 SUB     EQU     2                       My SUB custom trap function.
 LINKIT  EQU     253                     Append user table to TRAP 14 table.
-TUTOR   EQU     228                     Go to TUTORl print prompt.
+TUTOR   EQU     228                     Go to TUTOR; print prompt.
 
-; Set up table entries for custom trap functions.
+; Set up table entries for custom trap functions. Note that if you
+; call this a second time without initializing TUTOR, it will link it
+; to the previous table entry, which is this one, causing an infinite
+; loop and breaking the code. A reset will reinitialize TUTOR.
 
         lea     NEWTBL,a0               Register A0 points to new table.
         move.b  #LINKIT,d7              Specify LINKIT function
         trap    #14                     Call trap function
 
 ; On return, A0 contains $FETTTTTT where TTTTTT points to old table.
+; We need to write this to the end of the table.
 
         move.l  a0,ENDTBL               Write link to old table.
 
@@ -28,8 +32,8 @@ TUTOR   EQU     228                     Go to TUTORl print prompt.
 
 ; Now try calling SUB.
 
-        move.l    #$12345678,d0         First value to be added.
-        move.l    #$00010002,d1         Second value to be added.
+        move.l    #$12345678,d0         First value to be subtracted.
+        move.l    #$00010002,d1         Second value to be subtracted.
         move.b  #SUB,d7                 Specify SUB function
         trap    #14                     Call trap function
 
@@ -58,7 +62,9 @@ sub    move.l d1,-(sp)                  Save original D1 value.
        move.l (sp)+,d1                  Restore original D1 value.
        rts                              Return (note: RTS, not RTE)
 
-; Table for custom trap functions.
+; Table for custom trap functions. Table entries are in the format
+; $UUSSSSSS where UU is the function number and SSSSSS is the address
+; of the routine.
 
         align 1                         Align on word boundary
 NEWTBL  DC.L    ADD<<24+add             Table entry for ADD.
