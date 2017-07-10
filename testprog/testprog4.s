d7c5d8bfaf5f64838079df1322dabc2bb14990a6
Jeff Tranter <tranter@pobox.com>
2017-07-09 22:07:48 -0400

    Add example of extending TUTOR trap functions.

---
+++ testprog/testprog4.s
@@ -0,0 +1,66 @@
+; Example of extending TUTOR trap #14 functions with a custom
+; function. See the TUTOR documentation for details.
+
+        ORG	$2000                   Located in RAM
+
+; TRAP 14 functions
+ADD     EQU     1                       My ADD custom trap function.
+SUB     EQU     2                       My SUB custom trap function.
+LINKIT  EQU     253                     Append user table to TRAP 14 table.
+TUTOR   EQU     228                     Go to TUTORl print prompt.
+
+; Set up table entries for custom trap functions.
+
+        lea     NEWTBL,a0               Register A0 points to new table.
+        move.b  #LINKIT,d7              Specify LINKIT function
+        trap    #14                     Call trap function
+
+; On return, A0 contains $FETTTTTT where TTTTTT points to old table.
+
+        move.l  a0,ENDTBL               Write link to old table.
+
+; Now try calling ADD.
+
+        move.l    #$12345678,d0         First value to be added.
+        move.l    #$00010002,d1         Second value to be added.
+        move.b  #ADD,d7                 Specify ADD function
+        trap    #14                     Call trap function
+
+; Now try calling SUB.
+
+        move.l    #$12345678,d0         First value to be added.
+        move.l    #$00010002,d1         Second value to be added.
+        move.b  #SUB,d7                 Specify SUB function
+        trap    #14                     Call trap function
+
+; Return to TUTOR now that we are done.
+
+        move.b  #TUTOR,d7               Go to TUTOR function.
+        trap    #14                     Call TRAP14 handler.
+
+; Simple function to add two longword values and return result.
+; Pass values to be added in D0 and D1. Returns result in D2.
+; Does not change D0 or D1.
+
+add    move.l d1,-(sp)                  Save original D1 value.
+       add.l  d0,d1                     Add values
+       move.l d1,d2                     Store result in D2.
+       move.l (sp)+,d1                  Restore original D1 value.
+       rts                              Return (note: RTS, not RTE)
+
+; Simple function to subtract two longword values and return result.
+; Pass values to be added in D0 and D1. Returns result in D2.
+; Does not change D0 or D1.
+
+sub    move.l d1,-(sp)                  Save original D1 value.
+       sub.l  d0,d1                     Subtract values
+       move.l d1,d2                     Store result in D2.
+       move.l (sp)+,d1                  Restore original D1 value.
+       rts                              Return (note: RTS, not RTE)
+
+; Table for custom trap functions.
+
+        align 1                         Align on word boundary
+NEWTBL  DC.L    ADD<<24+add             Table entry for ADD.
+        DC.L    SUB<<24+sub             Table entry for SUB
+ENDTBL  DS.L    1                       Link to old table will be stored here.
