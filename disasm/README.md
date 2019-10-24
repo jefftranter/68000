94b45f7a717cb1ef2e891d37dc52684b20a2e8ee
Jeff Tranter <tranter@pobox.com>
2019-10-23 20:37:18 -0400

    Start of disassembler program.

---
+++ disasm/README.md
@@ -0,0 +1,4 @@
+This is (or will be) a disassembler for the Motorola 68000
+microprocessor. Written in Python, it uses data in CSV format based on
+the addressing mode table from the Motorola MC68000 16-/32-bit
+Microprocessor Programming Reference Card.
---
+++ disasm/testprog1.s
@@ -0,0 +1,50 @@
+        org	$1000
+
+start:
+        move.b  d1,d2
+        move.w  d3,d4
+        move.l  a1,a2
+
+        move.w  d0,d0
+        move.w  a0,d0
+        move.w  (a0),d0
+        move.w  (a0)+,d0
+        move.w  -(a0),d0
+        move.w  ($10,a0),d0
+        move.w  ($10,a0,d0),d0
+        move.w  ($10,a0,d0),d0
+        move.w  $12345678,d0
+        move.w  #1,d0
+        move.w  ($10,pc),d0
+        move.w  ($10,pc,d0),d0
+        move.w  ($1000,pc),d0
+
+        move.w  d0,d0
+        move.w  d0,a0
+        move.w  d0,(a0)
+        move.w  d0,(a0)+
+        move.w  d0,-(a0)
+        move.w  d0,($10,a0)
+        move.w  d0,($10,a0,d0)
+        move.w  d0,($10,a0,d0)
+        move.w  d0,$12345678
+
+        ori     #$0f,ccr
+        ori     #$aa,sr
+        ori.b   #$01,d0
+        ori.w   #$0102,d0
+        ori.l   #$01020304,d0
+        ori.b   #$ff,$12
+        ori.w   #$ff,$1234
+        ori.l   #$ff,$12345678
+
+        abcd.b  d1,d2
+        abcd.b  -(a2),-(a3)
+
+        nop
+        move.b  d1,d2
+        move.w  a1,d3
+        move.l  d4,a2
+        bra.s   end
+        bra.w   start
+end:    jmp     start
