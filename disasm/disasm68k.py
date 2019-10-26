e367b0d34641e143d921dd83b82e22aa4c1a68e6
Jeff Tranter <tranter@pobox.com>
2019-10-26 19:11:31 -0400

    Add more code.

--- disasm/disasm68k.py
+++ disasm/disasm68k.py
@@ -15,8 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import csv
+import re
+import sys
 
 with open('opcodetable.csv', newline='') as csvfile:
     table = list(csv.DictReader(csvfile))
@@ -28,11 +29,38 @@ with open('opcodetable.csv', newline='') as csvfile:
 for row in table:
     print(row["Mnemonic"], row["Bit Pattern"])
 
-# Convert bit pattern to 16-bit value and bitmask, e.g.
-# pattern: 1101 RRR1 1000 1rrr
-#   value: 1101000110001000
-#    mask: 1111000111111000
-# Opcode matches pattern if opcode AND mask equals value
+    # Make sure pattern matches XXXX XXXX XXXX XXXX
+    if not re.match(r"^.... .... .... ....$", row["Bit Pattern"]):
+        print("Bad bit pattern:", row["Bit Pattern"])
+        sys.exit(1)
+        
+    pattern = row["Bit Pattern"]
+    value = ""
+    mask = ""
+
+    for pos in [0, 1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 18]:
+      if pattern[pos] in ("0", "1"):
+          value += pattern[pos]
+          mask += "1"
+      else:
+          value += "0"
+          mask += "0"
+
+    value = int(value, 2)
+    mask = int(mask, 2)
+
+    print("pattern =", pattern)
+    print("  value = {0:016b}".format(value))
+    print("   mask = {0:016b}".format(mask))
+
+    # Convert bit pattern to 16-bit value and bitmask, e.g.
+    # pattern: 1101 RRR1 1000 1rrr
+    #   value: 1101000110001000
+    #    mask: 1111000111111000
+    # Opcode matches pattern if opcode AND mask equals value
+
+
+
 
 sys.exit(0)
 
