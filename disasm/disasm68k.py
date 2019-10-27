658bed1fa4d307b81ac2f83fda0ec4cf4f87ab31
Jeff Tranter <tranter@pobox.com>
2019-10-26 21:31:17 -0400

    More prototyping.

--- disasm/disasm68k.py
+++ disasm/disasm68k.py
@@ -19,39 +19,28 @@ import csv
 import re
 import sys
 
+# Open CSV file of opcodes and read into table
+
 with open('opcodetable.csv', newline='') as csvfile:
     table = list(csv.DictReader(csvfile))
 
-#print(table)
-#print(table[3])
-#print(table[3]["Mnemonic"])
+# Do validity check on table entries and calculate bitmask and value
+# for each opcode so we can quicly test opcode for matches in the
+# table.
 
 for row in table:
-    print(row["Mnemonic"], row["Bit Pattern"])
 
-    # Make sure pattern matches XXXX XXXX XXXX XXXX
+    # Validity check: Mnemonic is not empty.
+    if row["Mnemonic"] == "":
+        print("Empty mnemonic entry in opcodetable.csv")
+        sys.exit(1)
+
+    # Validity check: At least one addressing mode has a length
+
+    # Bit Pattern matches XXXX XXXX XXXX XXXX
     if not re.match(r"^.... .... .... ....$", row["Bit Pattern"]):
         print("Bad bit pattern:", row["Bit Pattern"])
         sys.exit(1)
-        
-    pattern = row["Bit Pattern"]
-    value = ""
-    mask = ""
-
-    for pos in [0, 1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 18]:
-      if pattern[pos] in ("0", "1"):
-          value += pattern[pos]
-          mask += "1"
-      else:
-          value += "0"
-          mask += "0"
-
-    value = int(value, 2)
-    mask = int(mask, 2)
-
-    print("pattern =", pattern)
-    print("  value = {0:016b}".format(value))
-    print("   mask = {0:016b}".format(mask))
 
     # Convert bit pattern to 16-bit value and bitmask, e.g.
     # pattern: 1101 RRR1 1000 1rrr
@@ -59,10 +48,24 @@ for row in table:
     #    mask: 1111000111111000
     # Opcode matches pattern if opcode AND mask equals value
 
+    pattern = row["Bit Pattern"]
+    value = ""
+    mask = ""
 
+    for pos in [0, 1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 15, 16, 17, 18]:
+        if pattern[pos] in ("0", "1"):
+            value += pattern[pos]
+            mask += "1"
+        else:
+            value += "0"
+            mask += "0"
 
+    # Convert value and mask to numbers and store in table.
+    value = int(value, 2)
+    mask = int(mask, 2)
+    row["Value"] = value
+    row["Mask"] = mask
 
-sys.exit(0)
 
 filename = "testprog1.bin"
 
@@ -82,5 +85,13 @@ while True:
     # Get op code
     opcode = ord(b1) * 256 + ord(b2)
 
-    #print("{0:02X}".format(opcode))
     print("{0:04X}".format(opcode))
+
+    for row in table:
+        value = row["Value"]
+        mask = row["Mask"]
+        mnemonic = row["Mnemonic"]
+
+        if (opcode & mask) == value:
+            print("Found match for", mnemonic)
+            break
