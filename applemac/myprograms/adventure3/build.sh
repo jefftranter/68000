faedcb399dbc6408a25f767e3c3327ed3fb2ddc0
Jeff Tranter <tranter@pobox.com>
2023-01-01 16:12:58 -0500

    Add first version of new adventure game.

---
+++ applemac/myprograms/adventure3/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm -rf build
+mkdir -p build
+cd build
+cmake .. -DCMAKE_TOOLCHAIN_FILE=${HOME}/git/Retro68-build/toolchain/m68k-apple-macos/cmake/retro68.toolchain.cmake 
+make
+#cp Adventure3.dsk /media/tranter/218E-8984/retro68/
+#umount /media/tranter/218E-8984
