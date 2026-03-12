fc8a1723efefb48708a1eff3902a3a1864412456
Jeff Tranter <tranter@pobox.com>
2026-03-12 11:11:58 -0400

    Update copyright date.

--- toolchain/disasm/disasm68k.py
+++ toolchain/disasm/disasm68k.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #
 # Motorola 68000 Disassembler
-# Copyright (c) 2019-2025 by Jeff Tranter <tranter@pobox.com>
+# Copyright (c) 2019-2026 by Jeff Tranter <tranter@pobox.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
